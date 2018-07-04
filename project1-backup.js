c15c1c9576c1a9ea4ace6826ab2707253423be4e
smiley-coyote <nick.a.alex@gmail.com>
2018-07-04 13:05:52 -0400

    chanded some stuff

--- project1-backup.js
+++ project1-backup.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
         dataType: 'jsonp',
         crossDomain: true,
         jsonp: 'json_callback',
-        url: 'http://www.giantbomb.com/api/search/?format=jsonp&api_key=47d89cf2776025d8ace3e66e641a4eb8bd066fc5&query=' + game
+        url: 'https://www.giantbomb.com/api/search/?format=jsonp&api_key=47d89cf2776025d8ace3e66e641a4eb8bd066fc5&query=' + game
       }).done(function(data) {
        var results = data.results
        console.log(results)
@@ -140,49 +140,49 @@ $(document).ready(function() {
        
     })
 
-    $.ajax({
+    // $.ajax({
 
-      // The 'type' property sets the HTTP method.
-      // A value of 'PUT' or 'DELETE' will trigger a preflight request.
-      type: 'GET',
+    //   // The 'type' property sets the HTTP method.
+    //   // A value of 'PUT' or 'DELETE' will trigger a preflight request.
+    //   type: 'GET',
     
-      // The URL to make the request to.
-      url: 'http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html',
+    //   // The URL to make the request to.
+    //   url: 'http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html',
     
-      // The 'contentType' property sets the 'Content-Type' header.
-      // The JQuery default for this property is
-      // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
-      // a preflight. If you set this value to anything other than
-      // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
-      // you will trigger a preflight request.
-      contentType: 'text/plain',
+    //   // The 'contentType' property sets the 'Content-Type' header.
+    //   // The JQuery default for this property is
+    //   // 'application/x-www-form-urlencoded; charset=UTF-8', which does not trigger
+    //   // a preflight. If you set this value to anything other than
+    //   // application/x-www-form-urlencoded, multipart/form-data, or text/plain,
+    //   // you will trigger a preflight request.
+    //   contentType: 'text/plain',
     
-      xhrFields: {
-        // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
-        // This can be used to set the 'withCredentials' property.
-        // Set the value to 'true' if you'd like to pass cookies to the server.
-        // If this is enabled, your server must respond with the header
-        // 'Access-Control-Allow-Credentials: true'.
-        withCredentials: false
-      },
+    //   xhrFields: {
+    //     // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+    //     // This can be used to set the 'withCredentials' property.
+    //     // Set the value to 'true' if you'd like to pass cookies to the server.
+    //     // If this is enabled, your server must respond with the header
+    //     // 'Access-Control-Allow-Credentials: true'.
+    //     withCredentials: false
+    //   },
     
-      headers: {
-        // Set any custom headers here.
-        // If you set any non-simple headers, your server must include these
-        // headers in the 'Access-Control-Allow-Headers' response header.
-      },
+    //   headers: {
+    //     // Set any custom headers here.
+    //     // If you set any non-simple headers, your server must include these
+    //     // headers in the 'Access-Control-Allow-Headers' response header.
+    //   },
     
-      success: function() {
-        // Here's where you handle a successful response.
-      },
+    //   success: function() {
+    //     // Here's where you handle a successful response.
+    //   },
     
-      error: function() {
-        // Here's where you handle an error response.
-        // Note that if the error was due to a CORS issue,
-        // this function will still fire, but there won't be any additional
-        // information about the error.
-      }
-    });
+    //   error: function() {
+    //     // Here's where you handle an error response.
+    //     // Note that if the error was due to a CORS issue,
+    //     // this function will still fire, but there won't be any additional
+    //     // information about the error.
+    //   }
+    // });
     
  
 
